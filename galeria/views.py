1c46f57c047e9d8c937f3663f523a0e5d3574a9a
Walisson Gomes da Silva <walissonwaal@gmail.com>
2023-06-30 17:18:23 -0300

    Criação do app usuarios, templates de login e cadastro, rotas e views.

--- galeria/views.py
+++ galeria/views.py
@@ -12,3 +12,12 @@ def imagem(request, foto_id):
     # Como último argumento do método render, um dicionário passando a variável (obj fotografia pelo id)
     # como props para a view (página) imagem.
     return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
+
+def buscar(request):
+    fotografias = Fotografia.objects.order_by("-data_fotografia").filter(publicada=True)
+    if "buscar" in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if nome_a_buscar:
+            fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
+
+    return render(request, 'galeria/buscar.html', {"cards": fotografias})
